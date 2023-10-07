c612a930df97248477d66e5a17f64313c213649a
Brahvim <69293652+Brahvim@users.noreply.github.com>
2023-10-07 13:45:41 +0530

    I think I'm literally done.

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,64 @@
 #include "Main.hpp"
 
+const DWORD g_procId = GetCurrentProcessId();
+
 int main() {
 	std::cout << "Start!" << std::endl;
+	std::map<HWND, RECT> windowsToRectsMap;
 
-	std::vector<HWND> windowsArray;
+	EnumWindows([](const _In_ HWND p_window, const _In_ LPARAM p_lParam) -> BOOL {
+		std::map<HWND, RECT> &windowsToRects = *(reinterpret_cast<std::map<HWND, RECT>*>(p_lParam));
+		DWORD procId = 0;
+		GetWindowThreadProcessId(p_window, &procId);
 
-	EnumWindows([](const _In_ HWND p_window, const _In_ LPARAM p_windowsArray) -> BOOL {
-		std::vector<HWND> &windows = *(reinterpret_cast<std::vector<HWND>*>(p_windowsArray));
-		windows.push_back(p_window);
+		EnumChildWindows(p_window, [](const _In_ HWND p_window, const _In_ LPARAM p_lParam) -> BOOL {
+			std::map<HWND, RECT> &windowsToRects = *(reinterpret_cast<std::map<HWND, RECT>*>(p_lParam));
+			DWORD procId = 0;
+			GetWindowThreadProcessId(p_window, &procId);
+
+			if (procId != g_procId) {
+				windowsToRects[p_window] = {};
+				GetWindowRect(p_window, &windowsToRects[p_window]);
+			}
+			return TRUE;
+		}, reinterpret_cast<LPARAM>(&windowsToRects));
+
+		if (procId != g_procId) {
+			windowsToRects[p_window] = {};
+			GetWindowRect(p_window, &windowsToRects[p_window]);
+		}
 		return TRUE;
-	}, reinterpret_cast<LPARAM>(&windowsArray));
-
-	std::cout << "We have `" << windowsArray.size() << "` windows.";
-	std::cout << "Press enter to make them spin!";
-
-	const DWORD procId = GetCurrentProcessId();
+	}, reinterpret_cast<LPARAM>(&windowsToRectsMap));
 
 
+	std::cout << "We have `" << windowsToRectsMap.size() << "` windows." << std::endl;
+	std::cout << "Press enter to make them spin." << std::endl;
 
+#pragma warning(suppress:6031)
+	std::getchar();
+
+	std::cout << "Spinning began!" << std::endl;
+
+	ULONGLONG timeVar = GetTickCount64();
+	const ULONGLONG endTime = timeVar + 5000;
+
+	while (timeVar < endTime) {
+		timeVar = GetTickCount64();
+
+		for (auto &element : windowsToRectsMap) {
+			const HWND window = element.first;
+			RECT rect = element.second;
+
+			GetWindowRect(window, &rect);
+
+			const LONG animTime = timeVar / ULLONG_MAX * SPINWIN_TIME_SCALE;
+
+			MoveWindow(window,
+				rect.left + (LONG) (sin(animTime) * SPINWIN_AMP),
+				rect.top + (LONG) (cos(animTime) * SPINWIN_AMP),
+				rect.right - rect.left,
+				rect.bottom - rect.top, true);
+		}
+	}
 
 }
--- Main.hpp
+++ Main.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
-#include <cassert>
 #include <cstdlib>
+#include <limits>
 #include <iostream>
+#include <map>
 #include <vector>
 
 #include <Windows.h>
+
+#define SPINWIN_TIME_SCALE 50
+#define SPINWIN_AMP 5
